f4c02e5c482cd39e62d62dec85d75738582ccbcd
Luis Felipe <felipe141099@gmial.com>
2019-12-16 07:19:35 -0500

    Materias Component dinamico

--- src/app/materias/materias.component.ts
+++ src/app/materias/materias.component.ts
@@ -9,17 +9,47 @@ import { DataService } from "../data.service";
 })
 export class MateriasComponent implements OnInit {
 
-  cursos:Curso[];
+  cursos: Curso[];
+  filasArray:number[];
+  matrix: Curso[][] = [];
 
-  constructor(private data:DataService) {
-    this.data.getData().subscribe(data=>{
-      
+  constructor(private data: DataService) {
+    this.data.getData().subscribe(data => {
+
       this.cursos = data;
-      console.log(this.cursos);
+    
+      this.tablaDinamica();
     })
   }
 
+  tablaDinamica(){
+    
+    let cantCursos: number = this.cursos.length;
+
+    let filas: number = Math.ceil(cantCursos/3);
+    //filas = cantCursos % 3 == 0 ? filas : filas + 1; //Otra forma de hacer Math.ceil()
+    this.filasArray = Array(filas).fill(0).map((_,index)=> index);
+
+    
+    let contCurso = 0;
+
+    for (let i = 0; i < filas; i++) {
+      this.matrix[i] = new Array(3);
+      for (let x = 0; x  < 3; x++) {
+        if (contCurso < cantCursos) {
+          this.matrix[i][x] = this.cursos[contCurso];
+          contCurso++;
+        }
+      }
+    }
+    console.log("Matrix ",this.matrix);
+
+  }
+
   ngOnInit() {
+    
+    
+
   }
 
 }
